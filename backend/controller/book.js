f0af2d1a9ff465be3c6f68f2c0b5b300aa1f279a
juangr18 <juanr970318@gmail.com>
2022-02-28 12:58:43 -0500

    Bugs fixed

--- backend/controller/book.js
+++ backend/controller/book.js
@@ -24,14 +24,12 @@ const listBook = async (req, res) => {
 
 const updateBook = async (req, res) => {
   let hasChange = await isChange(req.body);
-  console.log(req.body);
   if (!hasChange)
     return res.status(400).send({ message: "No change in the contact." });
   const bookEdit = await book.findByIdAndUpdate(req.body._id, {
     phoneNumber: req.body.phoneNumber,
     cellNumber: req.body.cellNumber,
   });
-  console.log(bookEdit);
   return bookEdit
     ? res.status(200).send({ message: "Conctact updated." })
     : res.status(500).send({ message: "Error updating conctact." });
