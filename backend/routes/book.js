0cb4f5e955af7bc42e0ea8df433d60460c945b34
juangr18 <juanr970318@gmail.com>
2022-02-28 08:17:35 -0500

    Implement code in routes for know limit on directory

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -7,5 +7,6 @@ router.post("/register", bookController.registerPhoneBook);
 router.get("/list/:name?", bookController.listBook);
 router.put("/update", bookController.updateBook);
 router.get("/available", bookController.availableContact);
+router.get("/limit", bookController.whatLimit);
 
 export default router;
